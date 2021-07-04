13febe94e7fe59f340cfb2443fa2758e1d045f72
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-07-04 14:03:31 +0530

    ready ...

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div className="container">
+            <div className="starter-template">
+                <h4>React-Django Template</h4>
+                <hr />
+
+                <h2>Vaibhav is DOPE</h2>
+                <img src="/static/images/vaibhav.jpg" className="align-center mx-auto" id="headshot" alt="dope vaibhav" />
+                <p class="lead">He is dope at everything.<br/> Except of making a girlfriend.</p>
+            </div>
+        </div>
+    )
+}
